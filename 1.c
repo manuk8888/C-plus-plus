8439b634feb57c873eeab7210889327dc8fa38a1
Manuk <90513022+manuk8888@users.noreply.github.com>
2025-02-20 12:26:56 +0300

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,36 @@
+{\rtf1\ansi\ansicpg1251\cocoartf2821
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 #include <iostream>\
+\
+int main() \{\
+  std::cout << "\uc0\u1047 \u1072 \u1076 \u1072 \u1085 \u1080 \u1077  \u8470  1\\n";\
+  std::cout << "==========\\n";\
+  std::cout << "Hello,\\n";\
+  std::cout << "World\\n";\
+  std::cout << "==========\\n";\
+  std::cout << "\\n";\
+  std::cout << "\\n";\
+  std::cout << "\\n";\
+  std::cout << "\uc0\u1047 \u1072 \u1076 \u1072 \u1085 \u1080 \u1077  \u8470  2\\n";\
+  std::cout << "==================\\n";\
+  std::cout << "||    Hello,    ||\\n";\
+  std::cout << "||    World     ||\\n";\
+  std::cout << "==================\\n";\
+  std::cout << "\\n";\
+  std::cout << "\\n";\
+  std::cout << "\\n";\
+  std::cout << "\uc0\u1047 \u1072 \u1076 \u1072 \u1085 \u1080 \u1077  \u8470  3\\n";\
+  std::cout << "==================\\n";\
+  std::cout << "||              ||\\n";\
+  std::cout << "||              ||\\n";\
+  std::cout << "||    Hello,    ||\\n";\
+  std::cout << "||    World     ||\\n";\
+  std::cout << "||              ||\\n";\
+  std::cout << "||              ||\\n";\
+  std::cout << "==================\\n";\
+\}}
